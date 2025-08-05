8e5522e8d8cdbc76d96bfd87f2d0db66231847c6
Ibn-Abdulsattar <attariusman813@gmail.com>
2025-08-06 00:19:24 +0500

    url change

--- frontend/src/landing_page/signup/ExplorAcounts.jsx
+++ frontend/src/landing_page/signup/ExplorAcounts.jsx
@@ -5,29 +5,32 @@ export default function ExploreAcounts() {
   const accounts = [
     {
       title: "Individual Account",
+      link: "https://main.dunuolnoll92w.amplifyapp.com",
       icon: <i className="fa-regular fa-circle-user"></i>,
       description: "Invest in equities, mutual funds, and derivatives",
     },
     {
       title: "HUF Account",
+      link: "https://main.dunuolnoll92w.amplifyapp.com/orders",
       icon: <i className="fa-solid fa-users-line"></i>,
       description: "Make tax-efficient investments for your family",
     },
     {
       title: "NRI Account",
+      link: "https://main.dunuolnoll92w.amplifyapp.com/holdings",
       icon: <i className="fa-solid fa-globe"></i>,
       description: "Invest in Indian markets while residing abroad",
     },
     {
       title: "Minor Account",
-      link: "",
+      link: "https://main.dunuolnoll92w.amplifyapp.com/positions",
       icon: <i className="fa-solid fa-child-reaching"></i>,
       description:
         "Teach your kids about investing early and save for their future",
     },
     {
       title: "Corporate / LLP / Partnership",
-      link: "",
+      link: "https://main.dunuolnoll92w.amplifyapp.com/funds",
       icon: <i className="fa-solid fa-building-columns"></i>,
       description:
         "Open accounts for business entities and manage surplus efficiently",
@@ -57,7 +60,7 @@ export default function ExploreAcounts() {
         <Grid container spacing={{ xs: 3, md: 4 }}>
           {accounts.map((account, idx) => (
             <Grid size={{ xs: 12, sm: 6, md: 4 }} key={idx}>
-              <Link href='https://main.dunuolnoll92w.amplifyapp.com/apps' target='_blank' sx={{textDecoration: "none"}}>
+              <Link href={account.link} target='_blank' sx={{textDecoration: "none"}}>
               <IndivAccount
                 icon={account.icon}
                 title={account.title}
